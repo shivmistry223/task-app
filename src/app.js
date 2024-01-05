0e1c75564b4baf0b98a82af0c374748df820f374
Shiv Mistry <shivmistry223@gmail.com>
2024-01-05 13:47:14 -0500

    Created POST api for task

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
-
+const Task = require('./models/task')
 const app = express()
 
 app.use(express.json())
@@ -16,6 +16,17 @@ app.post('/users', (req, res) => {
         res.status(400).send(err.message)
     })
 })
+
+
+app.post('/tasks', (req, res) => {
+    const task= new Task(req.body)
+    task.save().then(() => {
+        res.send(task)
+    }).catch((err) => {
+        res.status(400).send(err.message)
+    })
+})
+
 
 app.listen(port,() => {
     console.log('Server started on ' + port)
